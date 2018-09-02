e4bdf9a861e55e54a3a06467cc579e4d0ffee9b0
Kayvan Arianpour <karianpour@gmail.com>
2018-09-02 09:09:10 +0430

    Initial Commit

---
+++ db/sql/V2018_08_24__create_db.sql
@@ -0,0 +1,13 @@
+create database xcpilots encoding = 'utf8' lc_collate = 'fa_IR.utf8' template template0;
+
+create table news (
+	id uuid primary key,
+	title text collate "fa_IR",
+	description text collate "fa_IR",
+	body text collate "fa_IR",
+	image_url text collate "fa_IR",
+	created_at timestamp not null,
+	updated_at timestamp,
+	expiration timestamp
+);
+
